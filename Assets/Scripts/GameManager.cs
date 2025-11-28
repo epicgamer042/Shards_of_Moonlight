eec13a26f77dd2fc42a438c90855ad3ef1da692c
IcedCaramelCoffee7 <bretyorkuire@gmail.com>
2025-11-28 17:23:46 -0500

    StartedChanges

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> gameObjects; // Assign Level Prefabs in the 
+
+    // Manage Level displayed
+
+    // Manage Player spawn in level (get spawn location object from level prefab)
+}
