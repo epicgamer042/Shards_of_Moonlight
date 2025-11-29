6640b8a82c6259f77e2d76394f3106741ccf9e9e
IcedCaramelCoffee7 <bretyorkuire@gmail.com>
2025-11-29 18:01:06 -0500

    Updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,34 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> gameObjects; // Assign Level Prefabs in the 
+    [SerializeField] private List<GameObject> levelPrefabs; // List of Level Prefabs, assigned in inspector
+    [SerializeField] private GameObject playerPrefab; // Player prefab, assigned in inspector
+    private GameObject currentPlayer;
 
-    // Manage Level displayed
+    private void LoadLevel(int index)
+    {
+        // Turn off all levels
+        foreach (GameObject level in levelPrefabs)
+        {
+            level.SetActive(false);
+        }
 
-    // Manage Player spawn in level (get spawn location object from level prefab)
+        // Enable the desired level
+        levelPrefabs[index].SetActive(true);
+
+        // Find PlayerSpawn inside the active level
+        Transform spawnPoint = levelPrefabs[index].transform.Find("PlayerSpawn");
+
+        // Destroy old player if it exists
+        if (currentPlayer != null)
+        {
+            Destroy(currentPlayer);
+        }
+
+        // Spawn new player and store reference
+        currentPlayer = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
 }
