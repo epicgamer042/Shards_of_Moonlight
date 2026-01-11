0da0206190835a2da00e32fe33cf9ce7d20bf295
IcedCaramelCoffee7 <bretyorkuire@gmail.com>
2026-01-11 13:59:34 -0500

    changes

--- Assets/UISelectionRestorer.cs
+++ Assets/UISelectionRestorer.cs
@@ -1,63 +1,78 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
-using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
 
-public class UISelectionRestorer : MonoBehaviour
+public class UIInputManager : MonoBehaviour
 {
-    public GameObject firstSelected;
+    private enum InputDevice { Mouse, Controller }
+    private InputDevice currentDevice;
+
+    private PlayerControls input;
     private GameObject lastSelected;
+    private Vector2 lastMousePos;
 
-    public static bool UsingGamepad { get; private set; }
-    public static bool UsingMouseKeyboard { get; private set; }
-
-
-    private void Start()
+    private void Awake()
     {
-        EventSystem.current.SetSelectedGameObject(firstSelected);
-        lastSelected = firstSelected;
+        input = new PlayerControls();
+        input.Enable();
+
+        lastSelected = EventSystem.current.firstSelectedGameObject;
+        lastMousePos = Mouse.current.position.ReadValue();
+
+        currentDevice = InputDevice.Controller;
     }
 
     private void Update()
     {
+        TrackLastSelected();
+        DetectMouseSwitch();
+        DetectControllerSwitch();
+    }
+
+    private void DetectMouseSwitch()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        if (mousePos != lastMousePos)
+        {
+            lastMousePos = mousePos;
+
+            if (currentDevice != InputDevice.Mouse)
+            {
+                currentDevice = InputDevice.Mouse;
+
+                // IMPORTANT: Do NOT clear selection here.
+                // Unity will automatically highlight the hovered button.
+            }
+        }
+    }
+
+    private void DetectControllerSwitch()
+    {
+        if (input.UI.Navigate.triggered ||
+            input.UI.Submit.triggered ||
+            input.UI.Cancel.triggered)
+        {
+            if (currentDevice != InputDevice.Controller)
+            {
+                currentDevice = InputDevice.Controller;
+
+                // Restore last controller-selected button
+                EventSystem.current.SetSelectedGameObject(lastSelected);
+            }
+        }
+    }
+
+    private void TrackLastSelected()
+    {
         var current = EventSystem.current.currentSelectedGameObject;
 
-        // If nothing is selected, restore the last known selection
-        if (current == null)
-        {
-            EventSystem.current.SetSelectedGameObject(lastSelected);
-        }
-        else
+        if (current != null &&
+            current != lastSelected &&
+            current.GetComponent<Selectable>() != null)
         {
             lastSelected = current;
         }
-        Debug.Log(UsingGamepad);
     }
-
-    private void OnEnable()
-    {
-        InputSystem.onEvent += OnInputEvent;
-    }
-
-    private void OnDisable()
-    {
-        InputSystem.onEvent -= OnInputEvent;
-    }
-
-    private void OnInputEvent(InputEventPtr eventPtr, InputDevice device)
-    {
-        if (device is Gamepad)
-        {
-            UsingGamepad = true;
-            UsingMouseKeyboard = false;
-        }
-        else if (device is Keyboard || device is Mouse)
-        {
-            UsingMouseKeyboard = true;
-            UsingGamepad = false;
-        }
-    }
-
-
 }
